b24991daff3f2cfec08182dc539d7e95e09dca55
Yoga Pratama <yogapr.dev@gmail.com>
2024-07-19 10:45:02 +0700

    fix: separate outline button

--- components/ui/card-overlay.tsx
+++ components/ui/card-overlay.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { cn } from '@/lib/utils';
 import { Project } from '@/app/_components/projects';
-import { ChevronRight } from 'lucide-react';
+import { OutlineButton } from './outline-button';
 import Link from 'next/link';
 
 export function CardOverlay({
@@ -24,7 +24,6 @@ export function CardOverlay({
         )}
         style={{
           backgroundImage: `url(${thumbnail})`,
-          // backgroundImage: `@/public/${thumbnail}`,
           backgroundSize: 'cover',
         }}
       >
@@ -77,46 +76,6 @@ const TechstackBadge = ({ value }: { value: string }) => {
   return (
     <div className="px-2 py-1 border font-semibold font-mono bg-zinc-800 rounded-lg text-xs">
       {value}
-    </div>
-  );
-};
-
-const OutlineButton = () => {
-  const [isHovered, setIsHovered] = useState<boolean>(false);
-  return (
-    <div
-      className="relative p-[2px] active:scale-100 rounded-xl transition-transform duration-300 ease-in-out overflow-hidden"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div
-        className={cn(
-          'absolute inset-0 rounded-xl',
-          isHovered
-            ? 'absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#4ade80_0%,#393BB2_30%,#4ade80_100%)]'
-            : 'bg-transparent'
-        )}
-      />
-      <div
-        className={cn(
-          'relative w-fit px-6 py-2 bg-zinc-950 rounded-[10px] transition-all duration-300 ease-in-out'
-        )}
-      >
-        <div
-          className={cn(
-            'flex items-center gap-2 text-xs transition-colors duration-300 ease-in-out font-semibold',
-            isHovered ? 'text-zinc-100' : 'text-zinc-300'
-          )}
-        >
-          Visit
-          <ChevronRight
-            className={cn(
-              'size-4 transition-transform duration-500 ease-in-out motion-reduce:transform-none',
-              isHovered && 'animate-accordion-right'
-            )}
-          />
-        </div>
-      </div>
     </div>
   );
 };
